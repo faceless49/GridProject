f77a40b5614f2de4034dff85db46afb4f34c6fb8
Egor K <digital.zeus@mail.ru>
2021-02-21 14:22:52 +0300

    another notebook

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,13 +3,4 @@ let btnText = document.querySelector('.btn-text');
 
 likeBtn.addEventListener('click', function() {
      likeBtn.classList.toggle('liked');
-     if (likeBtn.className == 'liked') {
-        btnText.innerHTML = 'FAVED'
-     }
 });
-
-/* document.addEventListener('keydown', function(key){
-   if(key.key === 'l' || key.key === 'L') {
-      likeBtn.classList.toggle('liked');
-   }
-}); */
