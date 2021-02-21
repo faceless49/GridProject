bfe7150f0b7347e2fe6c4892a9f141980b0c26af
Egor K <digital.zeus@mail.ru>
2021-02-21 08:31:48 +0300

    fix sizes blocks

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,11 @@
-let likeBtn = document.querySelector('.btn');
+let likeBtn = document.querySelector('.btn-like');
+let btnText = document.querySelector('.btn-text');
 
 likeBtn.addEventListener('click', function() {
      likeBtn.classList.toggle('liked');
+     if (likeBtn.className == 'liked') {
+        btnText.innerHTML = 'FAVED'
+     }
 });
 
 /* document.addEventListener('keydown', function(key){
--- src/js/script.js
+++ src/js/script.js
@@ -4,9 +4,3 @@ let btnText = document.querySelector('.btn-text');
 likeBtn.addEventListener('click', function() {
      likeBtn.classList.toggle('liked');
 });
-
-/* document.addEventListener('keydown', function(key){
-   if(key.key === 'l' || key.key === 'L') {
-      likeBtn.classList.toggle('liked');
-   }
-}); */
